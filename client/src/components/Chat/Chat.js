14a4c492a3fadd247e68c11520145278814aec7c
AkramDev <akram.esaaidi@usmba.ac.ma>
2023-05-28 00:24:42 +0100

    update backend url

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -9,7 +9,7 @@ import Input from '../Input/Input';
 
 import './Chat.css';
 
-const ENDPOINT = 'https://react-chatone-app.herokuapp.com/';
+const ENDPOINT = 'https://chat-app-wqe6.onrender.com/';
 
 let socket;
 
